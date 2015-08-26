1338bbed3fe6c588e9992259f8e5a00a3fe9fd53
redaphid <a@octoblu.com>
2015-08-26 16:24:59 -0700

    states, templates, images

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-var app = angular.module('jade-is-wrong', []);
-app.directive('blinky', function(){
-  return {  
-    template: '<div>yo</div>',
+'use strict';
+var app = angular.module('jade-is-wrong', ['ui.router']);
+//TODO - remove checkLogin function
+// create the module and name it octobluApp
+app.config(function ($stateProvider, $locationProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/jade');
+  $locationProvider.html5Mode({ enabled: true, requireBase: false});  
+  $stateProvider
+    .state('jade', {
+      url: '/jade',
+      templateUrl: '/pages/jade.html'      
+    })
+    .state('jade.is', {
+      url: '/is',
+      templateUrl: '/pages/is.html'      
+    })
+    .state('jade.is.wrong', {
+      url: '/wrong',
+      templateUrl: '/pages/wrong.html'
+    });
     
-    // everything else as normal
-    scope: {
-      radius: '@',
-    }
-  };
+    $stateProvider
+      .state('aaron', {
+        url: '/aaron',
+        templateUrl: '/pages/aaron.html'      
+      })
+      .state('aaron.is', {
+        url: '/is',
+        templateUrl: '/pages/is.html'      
+      })
+      .state('jade.is.right', {
+        url: '/right',
+        templateUrl: '/pages/right.html'
+      });
 });
